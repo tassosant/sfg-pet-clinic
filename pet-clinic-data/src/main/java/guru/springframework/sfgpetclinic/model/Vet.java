ec0bec446669412f0f01803a86f19f65c0d5be97
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2024-01-11 21:17:53 +0200

    Added Specialties Map Impl

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -4,13 +4,13 @@ import java.util.Set;
 
 public class Vet extends Person{
 
-    private Set<Specialty> specialties;
+    private Set<Speciality> specialties;
 
-    public Set<Specialty> getSpecialties() {
+    public Set<Speciality> getSpecialties() {
         return specialties;
     }
 
-    public void setSpecialties(Set<Specialty> specialties) {
+    public void setSpecialties(Set<Speciality> specialties) {
         this.specialties = specialties;
     }
 }
